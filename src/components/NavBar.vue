dfef9e77aa379271d77d9ee028b4c0a852dbd866
pv-luizalmeida <125695179+pv-luizalmeida@users.noreply.github.com>
2024-11-09 16:13:34 -0300

    acamps

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -217,10 +217,10 @@ export default {
         id: '/#quemsomos',
         title: 'Quem somos'
       },
-      {
-        id: '/acamps',
-        title: 'Acamps'
-      },
+      // {
+      //   id: '/acamps',
+      //   title: 'Acamps'
+      // },
       // {
       //   id: 'https://api.whatsapp.com/send?phone=5535999819980&text=Ol%C3%A1%2C%20preciso%20de%20ajuda',
       //   title: 'Contato'
